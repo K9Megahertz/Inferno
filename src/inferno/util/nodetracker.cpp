d6f48609649e606eadb3f9d46d0a6070137fceca
k9megahertz <k9megahertz@yahoo.com>
2026-08-05 18:04:02 -0700

    Fixed some not threadsafe areas with Tensor ID's and adding/removing from NodeTracker. Temporary fix on CachingAllocator to avoid reusing memblocks that the gpu is not done using yet.

--- src/inferno/util/nodetracker.cpp
+++ src/inferno/util/nodetracker.cpp
@@ -22,7 +22,6 @@ namespace Inferno {
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     void NodeTracker::addID(int id) {
-        std::lock_guard<std::mutex> lock(m_mutex);
         // Add with empty name by default
         idNameMap[id] = "";
     }
